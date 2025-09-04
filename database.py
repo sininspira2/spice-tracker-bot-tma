6c1833cf4a9b266e4f21bb30fad4f953af8e4dae
sininspira2 <37672276+sininspira2@users.noreply.github.com>
2025-09-04 16:01:23 -0400

    Update database.py

--- database.py
+++ database.py
@@ -335,15 +335,15 @@ class Database:
                                         operation="withdrawal", sand_amount=sand_amount, target_user_id=target_user_id, error=str(e))
                 raise e
 
-    async def add_expedition_participant(self, expedition_id, user_id, username, sand_amount, melange_amount, leftover_sand, is_harvester=False):
+    async def add_expedition_participant(self, expedition_id, user_id, username, sand_amount, melange_amount, is_harvester=False):
         """Add a participant to an expedition"""
         start_time = time.time()
         async with self._get_connection() as conn:
             try:
                 await conn.execute('''
-                    INSERT INTO expedition_participants (expedition_id, user_id, username, sand_amount, melange_amount, leftover_sand, is_harvester)
-                    VALUES ($1, $2, $3, $4, $5, $6, $7)
-                ''', expedition_id, user_id, username, sand_amount, melange_amount, leftover_sand, is_harvester)
+                    INSERT INTO expedition_participants (expedition_id, user_id, username, sand_amount, melange_amount, is_harvester)
+                    VALUES ($1, $2, $3, $4, $5, $6)
+                ''', expedition_id, user_id, username, sand_amount, melange_amount, is_harvester)
                 
                 await self._log_operation("insert", "expedition_participants", start_time, success=True, 
                                         expedition_id=expedition_id, user_id=user_id, sand_amount=sand_amount, is_harvester=is_harvester)
@@ -379,7 +379,7 @@ class Database:
         async with self._get_connection() as conn:
             try:
                 # Get expedition details first
-                expedition_row = await conn.fetchrow('''
+                ition_row = await conn.fetchrow('''
                     SELECT initiator_id, initiator_username, total_sand, guild_cut_percentage, 
                            sand_per_melange, created_at
                     FROM expeditions 
@@ -815,6 +815,7 @@ class Database:
             except Exception as e:
                 await self._log_operation("select_join", "deposits_users", start_time, success=False, 
                                         error=str(e))
+
                 raise e
 
 # test comment
